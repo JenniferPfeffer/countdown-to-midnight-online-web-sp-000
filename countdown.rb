dfd83e9bf6ab35fadf1faba758658b5501b3c83f
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-23 20:49:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,15 @@
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0 
   puts "#{seconds_to_midnight} SECOND(S)!" 
-  integer -= 1
+  seconds_to_midnight -= 1
 end
   return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
